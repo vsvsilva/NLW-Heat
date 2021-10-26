b901a85fc35fe021fc6ef4ba80fddf315a4b4525
Vitor <vitor_ovitor@outlook.com>
2021-10-26 16:30:44 -0300

    back-end

---
+++ node_heat/src/controllers/GetLast3MessagesController.ts
@@ -0,0 +1,16 @@
+import { Request, response, Response } from "express"
+import { AuthenticateUserService } from "../services/AuthenticateUserService"
+import { CreateMessageService } from "../services/CreateMessageService"
+import { GetLast3MessagesService } from "../services/GetLast3MessagesService"
+
+class GetLast3MessagesController {
+    async handle(request: Request, response: Response) {
+        
+        const service = new GetLast3MessagesService()
+        const result = await service.execute()
+
+        return response.json(result)
+    }
+}
+
+export { GetLast3MessagesController }
