bf83f27d25a391eb6d71806783bd0a4579cd575e
Vitor <vitor_ovitor@outlook.com>
2021-10-27 14:34:04 -0300

    front-end

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,7 +1,41 @@
 import styles from './styles.module.scss'
+import logoImg from '../../assets/logo.svg'
 
 export function MessageList() {
     return(
-        <h1>MessageList</h1>
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>alguma mensagem maneira, só pra testar mesmo!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vsvsilva.png" alt="Vitor Souza" />
+                        </div>
+                        <span>Vitor Souza</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>alguma mensagem maneira, só pra testar mesmo!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vsvsilva.png" alt="Vitor Souza" />
+                        </div>
+                        <span>Vitor Souza</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>alguma mensagem maneira, só pra testar mesmo!</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vsvsilva.png" alt="Vitor Souza" />
+                        </div>
+                        <span>Vitor Souza</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
