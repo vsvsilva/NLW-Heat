8ca89fbf9ad926186d388fc30e50a10eede5b236
Vitor <vitor_ovitor@outlook.com>
2021-11-03 16:56:40 -0300

    assets

--- nlwheatapp/src/screens/Home/index.tsx
+++ nlwheatapp/src/screens/Home/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { View, Text } from 'react-native'
+import { styles } from './styles'
 
 export function Home(){
     return(
-        <View>
+        <View style={styles.container}>
             <Text>olá react native</Text>
         </View>
     )
