b3ff69ed7420e80fc5fa6ac95acf59a278acf084
Vitor <vitor_ovitor@outlook.com>
2021-11-24 15:55:55 -0300

    animação

--- nlwheatapp/src/components/Message/index.tsx
+++ nlwheatapp/src/components/Message/index.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import { UserPhoto } from '../UserPhoto'
 import { styles } from './styles'
+import { MotiView } from 'moti'
 
 export type MessageProps = {
     id: string
@@ -18,7 +19,11 @@ type Props = {
 
 export function Message({ data }: Props) {
     return (
-        <View style={styles.container}>
+        <MotiView
+            from={{ opacity:0, translateY: -50}}
+            animate={{opacity: 1, translateY: 0}}
+            transition={{type: 'timing', duration: 700}}
+            style={styles.container}>
             <Text style={styles.message}>
                 {data.text}
             </Text>
@@ -33,6 +38,6 @@ export function Message({ data }: Props) {
                 </Text>
             </View>
 
-        </View>
+        </MotiView>
     )
 }
